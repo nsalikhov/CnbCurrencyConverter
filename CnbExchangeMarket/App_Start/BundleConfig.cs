e7ae34f4c99d2da8c624d8ed85cc0c5089a350bc
Nail Salikhov <nail.salikhov@hotmail.com>
2016-05-16 01:37:56 +0300

    Datetime picker control added.

--- CnbExchangeMarket/App_Start/BundleConfig.cs
+++ CnbExchangeMarket/App_Start/BundleConfig.cs
@@ -12,11 +12,13 @@ namespace CnbExchangeMarket
 				new ScriptBundle("~/js").Include(
 					"~/Scripts/jquery-{version}.js",
 					"~/Scripts/bootstrap.js",
-					"~/Scripts/knockout-{version}.js"));
+					"~/Scripts/knockout-{version}.js",
+					"~/Scripts/jquery.datetimepicker.full.min.js"));
 
 			bundles.Add(
 				new StyleBundle("~/css").Include(
 					"~/Content/bootstrap.css",
+					"~/Content/jquery.datetimepicker.min.css",
 					"~/Content/site.css"));
 		}
 	}
